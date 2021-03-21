b130296236e55f10dbf6fd4676996e300a5dbef7
Karine Rodrigues <karinkau13@gmail.com>
2021-03-21 17:50:17 -0300

    Commit full play
    Jogo botão maluco.

---
+++ pacote-download/Botão_maluco/Botão_maluco/Form1.Designer.vb
@@ -0,0 +1,112 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBotaoMaluco
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmBotaoMaluco))
+        Me.btnCliqueAqui = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnCliqueAqui
+        '
+        Me.btnCliqueAqui.BackColor = System.Drawing.Color.Cyan
+        Me.btnCliqueAqui.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btnCliqueAqui.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnCliqueAqui.Font = New System.Drawing.Font("Arial Narrow", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCliqueAqui.Image = CType(resources.GetObject("btnCliqueAqui.Image"), System.Drawing.Image)
+        Me.btnCliqueAqui.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnCliqueAqui.Location = New System.Drawing.Point(233, 153)
+        Me.btnCliqueAqui.Name = "btnCliqueAqui"
+        Me.btnCliqueAqui.Size = New System.Drawing.Size(449, 61)
+        Me.btnCliqueAqui.TabIndex = 1
+        Me.btnCliqueAqui.Text = "CLIQUE AQUI !  se você conseguir."
+        Me.btnCliqueAqui.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Algerian", 27.75!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(226, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(456, 41)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "JOGO DA CONCENTRAÇÃO"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Label2.Font = New System.Drawing.Font("Calibri", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 184)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(181, 116)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "   Botão maluco" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & " " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "   O jogador tem " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "que clicar em cima " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "do botão indicado " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "p" & _
+    "ara ver o que acontece."
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Calibri", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Blue
+        Me.Label3.Location = New System.Drawing.Point(414, 347)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(268, 19)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Karin kau Produções - (38) 9.9125-5433"
+        '
+        'frmBotaoMaluco
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Khaki
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(713, 375)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnCliqueAqui)
+        Me.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmBotaoMaluco"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Botão maluco"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnCliqueAqui As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+End Class
---
+++ pacote-download/Botão_maluco/Botão_maluco/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class frmBotaoMaluco
+    'Jogo simples elaborado 14-01-2017
+
+    'Nome:  BOTÃO MALUCO
+    'Função : o jogador tentará clicar em cima do botão.
+    'Autor :  Karine s m Rodrigues - (38) 9.9125-5433
+    'Atualização : 
+
+    '#######################################################################
+
+    Private Sub btnCliqueAqui_MouseMove(sender As Object, e As System.Windows.Forms.MouseEventArgs) Handles btnCliqueAqui.MouseMove
+        If btnCliqueAqui.Top < 100 Then
+            btnCliqueAqui.Top = 250
+            btnCliqueAqui.Text = "NÃO DESISTA !  tente mais uma vez."
+            btnCliqueAqui.BackColor = Color.Violet
+        Else
+            btnCliqueAqui.Top = 70
+            btnCliqueAqui.Text = "CLIQUE AQUI !  se você conseguir."
+            btnCliqueAqui.BackColor = Color.Turquoise
+        End If
+    End Sub
+End Class
+'   Karin Kau Produções
